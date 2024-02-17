55abcde2c9aa5fd739ae0544e9dc6a9d5d66d234
Josue Espinal <josueespinalsanz@gmail.com>
2024-02-16 19:01:05 -0600

    Add sorting and cache control to product category API so dynamic changes reflect properly.

--- src/app/api/products/category/route.ts
+++ src/app/api/products/category/route.ts
@@ -4,15 +4,24 @@ import { NextRequest, NextResponse } from "next/server";
 //* FETCH ALL PRODUCTS BY CATEGORY
 export const GET = async (req: NextRequest) => {
     const { searchParams } = new URL(req.url);
-    const cat = searchParams.get("cat");
+    const cat = searchParams.get("cat") || "";
+
 
     try {
         const products = await prisma.product.findMany({
             where: {
-                ...(cat ? { catSlug: cat } : { isFeatured: true }),
+                catSlug: cat,
+                isVisible: true,
             },
+            orderBy: [
+                {
+                    title: "asc",
+                },
+            ]
         });
-        return new NextResponse(JSON.stringify(products), { status: 200 });
+        const response = new NextResponse(JSON.stringify(products), { status: 200 });
+        response.headers.set("Cache-Control", "no-store");
+        return response;
     } catch (err) {
         console.log(err);
         return new NextResponse(
